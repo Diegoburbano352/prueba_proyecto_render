0711ab00047b30dd7d9b1e6b9c61b43c40600acb
DiegoBurbano352 <dburbanog1@miumg.edu.gt>
2024-02-11 23:09:29 -0600

    4

--- server.js
+++ server.js
@@ -18,12 +18,17 @@ db.sequelize.sync({ force: false }).then(() => {
   console.log("La base de datos ha sido reiniciada (force: true)");
 });
 
-app.use('/api/users', userRoutes);
+// Configuración para manejar las rutas en React
+const root = path.join(__dirname, 'build');
 
+// Middleware para manejar las rutas en React antes de las rutas específicas
+app.use(express.static(root));
+
+// Rutas específicas de tu aplicación
+app.use('/api/users', userRoutes);
 app.get("/", (req, res) => {
   res.json({ message: "Bienvenido a nuestra página." });
 });
-
 require("./app/routes/client.routes")(app);
 require("./app/routes/pet.routes")(app);
 require("./app/routes/pet_user.routes")(app);
@@ -36,12 +41,6 @@ require("./app/routes/invoice.routes")(app);
 require("./app/routes/client_user.routes")(app);
 require("./app/routes/reservation_user.routes")(app);
 require("./app/routes/user_admin.routes")(app);
-
-// Configuración para manejar las rutas en React
-const root = path.join(__dirname, 'build');
-
-// Middleware para manejar las rutas en React antes de la ruta '*' (Wildcard)
-app.use(express.static(root));
 
 // Ruta para manejar todas las demás solicitudes y devolver la página principal de React
 app.get('*', (req, res) => {
